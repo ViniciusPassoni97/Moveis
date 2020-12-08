d0d2e935c6e7170bc45cd7e5ac20064e8b604def
ViniciusPassoni97 <43391455+ViniciusPassoni97@users.noreply.github.com>
2020-12-07 21:43:19 -0300

    Terminando Done

--- src/components/Done/index.js
+++ src/components/Done/index.js
@@ -5,26 +5,26 @@ import './style.css';
 function Done() {
   return (
     <div className='done'>
-        <div className='done-info'>
-            <p className=''>980</p>
-            <p className=''>Project</p>
+        <div className='done-info-message'>
+            <p className='done-info-message-number'>+3000 Completed Work</p>
+            <p className='done-info-message-info'>Which WE have Successfully Done</p>
         </div>
         <div className='done-info'>
             <div className='done-info-group'>
-                <p className=''>520</p>
-                <p className=''>Happy Client</p>
+                <p className='number'>980</p>
+                <p className='number-info'>Project</p>
             </div>
             <div className='done-info-group'>
-                <p className=''></p>
-                <p className=''></p>
+                <p className='number'>520</p>
+                <p className='number-info'>Happy Client</p>
             </div>
             <div className='done-info-group'>
-                <p className=''>330</p>
-                <p className=''>Winners</p>
+                <p className='number'>330</p>
+                <p className='number-info'>Winners</p>
             </div>
             <div className='done-info-group'>
-                <p className=''>120</p>
-                <p className=''>Recoment</p>
+                <p className='number'>120</p>
+                <p className='number-info'>Recoment</p>
             </div>
         </div>
     </div>
