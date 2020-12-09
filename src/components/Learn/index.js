8e44b72ecd2cc9720f65141629f53ccf4e7b5466
ViniciusPassoni97 <43391455+ViniciusPassoni97@users.noreply.github.com>
2020-12-09 00:02:17 -0300

    Iniciando com o Layout Responsivo

--- src/components/Learn/index.js
+++ src/components/Learn/index.js
@@ -5,9 +5,6 @@ import'./style.css';
 function Learn() {
     return (
         <div className='about'>
-            <div className='about-image'>
-                <img className='about-image-img' src={MaskGroup} alt='Logo'/>
-            </div>
             <div className='about-info'>
                 <p className='about-info-header'>ABOUT UT</p>
                 <p className='about-info-title'>Adalah menjadi satu fakta bahawa pembaca akan terganggu oleh text yang boleh</p>
@@ -16,6 +13,9 @@ function Learn() {
                  Ia telah bertahan bukan hanya selama lima kurun,
                  tetapi telah melonjak ke era typesetting elektronik, dengan tiada perubahan ketara</p>
                  <button className='about-info-btn'>Learn More</button>
+            </div>
+            <div className='about-image'>
+                <img className='about-image-img' src={MaskGroup} alt='Logo'/>
             </div>
         </div>
    );
